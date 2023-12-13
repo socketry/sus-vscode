659316338003681cabde4cac294b9f1272332939
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-12-13 21:25:38 +1300

    Fix "after" not defined.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,13 +1,8 @@
 import * as assert from 'assert';
-
 import * as vscode from 'vscode';
 //import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
-	after(() => {
-		vscode.window.showInformationMessage('All tests done!');
-	});
-
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
